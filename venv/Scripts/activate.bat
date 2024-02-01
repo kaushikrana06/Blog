4f5e404da9c719c186edc61a00d3dcd9a62a6eb8
kaushikrana06 <kaushik.rana.cse24@heritageit.edu.in>
2024-02-01 09:55:05 +0530

    initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sonad\Downloads\new\new\test\my_fastapi_blog\venv
+set VIRTUAL_ENV=C:\Users\kaush\Downloads\my_fastapi_blog\my_fastapi_blog\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
